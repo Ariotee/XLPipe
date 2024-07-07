a8d9cc117738ae85ed127ba34d0d96c12f85a447
SoloTufi <mat_990011@mail.ru>
2024-07-07 19:32:30 +0700

    #

---
+++ src/app/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+
+import './scss/all.scss';
+
+import Header from '../components/header/Header';
+import HomePage from '../pages/Home';
+import Catalog from '../pages/Catalog';
+
+function App() {
+	return (
+		<>
+			<div className='pos-r'>
+				<Header />
+			</div>
+
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/catalog' element={<Catalog />} />
+			</Routes>
+		</>
+	);
+}
+
+export default App;
